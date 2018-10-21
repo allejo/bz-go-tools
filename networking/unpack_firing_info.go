e9cad8e5dfd78d2843f9a073d0aa2b3c8ba21e3d
Vladimir Jimenez <allejo@me.com>
2018-10-20 23:00:57 -0700

    Correctly unpack flag data in MsgShotBegin

--- networking/unpack_firing_info.go
+++ networking/unpack_firing_info.go
@@ -7,14 +7,14 @@ import (
 type FiringInfoData struct {
 	TimeSent float32  `json:"timeSent"`
 	Shot     ShotData `json:"shot"`
-	Flag     FlagData `json:"flag"`
+	Flag     string   `json:"flag"`
 	Lifetime float32  `json:"lifetime"`
 }
 
 func UnpackFiringInfo(buf *bytes.Buffer) (packet FiringInfoData) {
 	packet.TimeSent = UnpackFloat(buf)
 	packet.Shot = UnpackShot(buf)
-	packet.Flag = UnpackFlag(buf)
+	packet.Flag = UnpackString(buf, 2)
 	packet.Lifetime = UnpackFloat(buf)
 
 	return
