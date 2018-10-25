e881f66376c30379408e3179de38a4d706edbf9a
allejo <allejo@me.com>
2018-10-24 22:26:46 -0700

    Correctly unpack MsgCaptureFlag with all its data

--- networking/msg_capture_flag.go
+++ networking/msg_capture_flag.go
@@ -6,13 +6,17 @@ import (
 )
 
 type MsgCaptureFlagPacket struct {
-	Type string `json:"type"`
-	Team uint16 `json:"team"`
+	Type     string `json:"type"`
+	PlayerID uint8  `json:"playerID"`
+	FlagID   uint16 `json:"flagID"`
+	Team     uint16 `json:"team"`
 }
 
 func (m *MsgCaptureFlagPacket) Unpack(buf *bytes.Buffer) (packet MsgCaptureFlagPacket) {
 	packet.Type = "MsgCaptureFlag"
 
+	binary.Read(buf, binary.BigEndian, &packet.PlayerID)
+	binary.Read(buf, binary.BigEndian, &packet.FlagID)
 	binary.Read(buf, binary.BigEndian, &packet.Team)
 
 	return
